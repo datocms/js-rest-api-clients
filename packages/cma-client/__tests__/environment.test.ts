099c8cad4de8f87f53fb6a71727d9188cb0fc91f
Irene Oppo <i.oppo@datocms.com>
2022-04-22 16:14:22 +0200

    Added environment test

---
+++ packages/cma-client/__tests__/environment.test.ts
@@ -0,0 +1,24 @@
+import { generateNewCmaClient } from './helpers/generateClients';
+
+describe('environments', () => {
+  it('all, find, fork, promote, destroy', async () => {
+    const client = await generateNewCmaClient();
+    const primaryEnvironment = await client.environments.find('main');
+
+    const forkedEnvironment = await client.environments.fork(
+      primaryEnvironment.id,
+      {
+        id: 'sandbox-test',
+      },
+    );
+
+    await client.environments.promote(forkedEnvironment.id);
+
+    await client.environments.promote(primaryEnvironment.id);
+
+    await client.environments.destroy(forkedEnvironment.id);
+
+    const environments = await client.environments.list();
+    expect(environments.length).toEqual(1);
+  });
+});
---
+++ packages/cma-client/__tests__/plugin.test.ts
@@ -0,0 +1,18 @@
+import { generateNewCmaClient } from './helpers/generateClients';
+
+describe('plugins', () => {
+  test('create, find, all, duplicate, update, destroy', async () => {
+    const client = await generateNewCmaClient();
+    const plugin = await client.plugins.create({
+      package_name: 'datocms-plugin-tag-editor',
+    });
+
+    await client.plugins.fields(plugin.id);
+
+    await client.plugins.update(plugin.id, {
+      parameters: { development_mode: true },
+    });
+
+    await client.plugins.destroy(plugin.id);
+  });
+});
