c1f32c1800335f16bdca715461d43d0cf6452f3e
Stefano Verna <s.verna@datocms.com>
2022-04-26 13:10:07 +0200

    Move stuff around

--- packages/cma-client-node/__tests__/upload.test.ts
+++ packages/cma-client-node/__tests__/upload.test.ts
@@ -23,4 +23,49 @@ describe('upload', () => {
 
     expect(upload.path.endsWith('dato.png')).toBeTruthy();
   });
+
+  test('references', async () => {
+    const itemType = await client.itemTypes.create({
+      name: 'Article',
+      api_key: 'article',
+      singleton: true,
+      modular_block: false,
+      sortable: false,
+      tree: false,
+      draft_mode_active: false,
+      ordering_direction: null,
+      all_locales_required: true,
+    });
+
+    await client.fields.create(itemType, {
+      label: 'Image',
+      field_type: 'file',
+      localized: false,
+      api_key: 'image',
+      validators: { required: {} },
+    });
+
+    // const path = await client.createUploadPath(
+    //   'https://www.w3.org/People/mimasa/test/imgformat/img/w3c_home.jpg',
+    // );
+    const path = 'www.foo.bar/whoa.txt';
+
+    const upload = await client.uploads.create({
+      path,
+    });
+
+    type RecordTestType = {
+      image: { uploadId: string };
+    };
+
+    await client.items.create({
+      ...({ image: { uploadId: upload.id } } as RecordTestType),
+      item_type: itemType,
+    });
+
+    const items = await client.uploads.references(upload.id);
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    expect((items[0].image as any).uploadId).toEqual(upload.id);
+  });
 });
