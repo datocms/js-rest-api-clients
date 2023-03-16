bba8b15491f09e7edd41be5368f5384b7bbbd732
Stefano Verna <s.verna@datocms.com>
2023-03-16 11:50:04 +0100

    Allow configuration

--- packages/rest-api-events/src/withEventsSubscription.ts
+++ packages/rest-api-events/src/withEventsSubscription.ts
@@ -1,13 +1,15 @@
 import { GenericClient } from './internalTypes';
 import { JobResultsFetcher } from './JobResultsFetcher';
+import { SubscriptionConfig } from './subscribeToEvents';
 
 type UnsubscribeToEventsFn = () => void;
 
 export async function withEventsSubscription<T extends GenericClient>(
   client: T,
+  config?: SubscriptionConfig,
 ): Promise<[T, UnsubscribeToEventsFn]> {
   const jobResultsFetcher = new JobResultsFetcher<T>(client);
-  await jobResultsFetcher.subscribeToEvents();
+  await jobResultsFetcher.subscribeToEvents(config);
   client.jobResultsFetcher = jobResultsFetcher.fetch;
   return [client, () => jobResultsFetcher.unsubscribeToEvents()];
 }
