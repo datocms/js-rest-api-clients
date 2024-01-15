f4e628329c686f7312334f0168bd53ee89f3cae4
Stefano Verna <s.verna@datocms.com>
2024-01-15 17:03:06 +0100

    Test schema_menu_item creation

---
+++ packages/cma-client-node/__tests__/schemaMenuItem.test.ts
@@ -0,0 +1,51 @@
+import { generateNewCmaClient } from '../../../jest-helpers/generateNewCmaClient';
+import { generateId } from '../../cma-client/src';
+
+describe('menu item', () => {
+  it.concurrent('create, find, list, update, destroy', async () => {
+    const client = await generateNewCmaClient();
+
+    const schemaMenuItem = await client.schemaMenuItems.create({
+      kind: 'item_type',
+      label: 'Editorial content',
+      position: 1,
+    });
+
+    expect(schemaMenuItem.label).toEqual('Editorial content');
+
+    const foundschemaMenuItems = await client.schemaMenuItems.find(
+      schemaMenuItem,
+    );
+    expect(foundschemaMenuItems.id).toEqual(schemaMenuItem.id);
+
+    const allSchemaMenuItems = await client.schemaMenuItems.list();
+    expect(allSchemaMenuItems).toHaveLength(1);
+
+    const updatedschemaMenuItems = await client.schemaMenuItems.update(
+      schemaMenuItem,
+      {
+        ...schemaMenuItem,
+        label: 'Updated',
+      },
+    );
+    expect(updatedschemaMenuItems.label).toEqual('Updated');
+
+    await client.schemaMenuItems.destroy(schemaMenuItem);
+    expect(await client.schemaMenuItems.list()).toHaveLength(0);
+  });
+
+  it.concurrent('create with explicit ID', async () => {
+    const client = await generateNewCmaClient();
+
+    const newId = generateId();
+
+    const schemaMenuItem = await client.schemaMenuItems.create({
+      id: newId,
+      kind: 'item_type',
+      label: 'Editorial content',
+      position: 1,
+    });
+
+    expect(schemaMenuItem.id).toEqual(newId);
+  });
+});
