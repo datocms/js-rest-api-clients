6d7c1baf1115a0999c762360e6aeb6d62436d821
Silvano Stralla <silvano.stralla@sistrall.it>
2024-04-17 17:37:08 +0200

    Format code

--- packages/cma-client/src/generated/Client.ts
+++ packages/cma-client/src/generated/Client.ts
@@ -143,7 +143,7 @@ export class Client {
       ...this.config,
       ...options,
       logFn: this.config.logFn || console.log,
-      userAgent: '@datocms/cma-client',
+      userAgent: '@datocms/cma-client v3.1.12-alpha.0',
       baseUrl: this.baseUrl,
       preCallStack: new Error().stack,
       extraHeaders: {
--- packages/dashboard-client/src/generated/Client.ts
+++ packages/dashboard-client/src/generated/Client.ts
@@ -112,7 +112,7 @@ export class Client {
       ...this.config,
       ...options,
       logFn: this.config.logFn || console.log,
-      userAgent: '@datocms/dashboard-client',
+      userAgent: '@datocms/dashboard-client v3.1.12-alpha.0',
       baseUrl: this.baseUrl,
       preCallStack: new Error().stack,
       extraHeaders: {
