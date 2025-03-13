2f32f29bbd0db06309f117f03bddb097c96d92d5
Stefano Verna <s.verna@datocms.com>
2025-03-13 08:36:05 +0100

    @datocms/cma-client-browser: Add client.uploads.updateFromFileOrBlob()

--- packages/cma-client-browser/src/resources/Upload.ts
+++ packages/cma-client-browser/src/resources/Upload.ts
@@ -26,6 +26,15 @@ export type CreateUploadFromFileOrBlobSchema = Omit<
   onProgress?: (info: OnUploadProgressInfo) => void;
 };
 
+export type UpdateUploadFromFileOrBlobSchema = Omit<
+  SimpleSchemaTypes.UploadUpdateSchema,
+  'path'
+> & {
+  fileOrBlob: File | Blob;
+  filename?: string;
+  onProgress?: (info: OnUploadProgressInfo) => void;
+};
+
 export default class Upload extends Resources.Upload {
   /**
    * Create a new upload using a browser File/Blob object
@@ -58,6 +67,49 @@ export default class Upload extends Resources.Upload {
         }
 
         return this.create({ ...other, path });
+      },
+      () => {
+        if (uploadPromise) {
+          uploadPromise.cancel();
+        } else {
+          isCanceledBeforeUpload = true;
+        }
+      },
+    );
+  }
+
+  /**
+   * Updates an upload asset using a browser File/Blob object
+   *
+   * Read more: https://www.datocms.com/docs/content-management-api/resources/upload/update
+   */
+  updateFromFileOrBlob(
+    uploadId: string | SimpleSchemaTypes.UploadData,
+    body: UpdateUploadFromFileOrBlobSchema,
+  ): CancelablePromise<SimpleSchemaTypes.Upload> {
+    let isCanceledBeforeUpload = false;
+    let uploadPromise: CancelablePromise<string> | undefined;
+
+    return makeCancelablePromise(
+      async () => {
+        if (isCanceledBeforeUpload) {
+          throw new CanceledPromiseError();
+        }
+
+        const { fileOrBlob, filename, onProgress, ...other } = body;
+
+        uploadPromise = uploadFileOrBlobAndReturnPath(this.client, fileOrBlob, {
+          filename,
+          onProgress,
+        });
+
+        const path = await uploadPromise;
+
+        if (onProgress) {
+          onProgress({ type: 'CREATING_UPLOAD_OBJECT' });
+        }
+
+        return this.update(uploadId, { ...other, path });
       },
       () => {
         if (uploadPromise) {
